13f528e5db0f733d38b2aaab79cc823572c68247
Feier Raul <raul.feier.9a.2021@cnaibrad.ro>
2022-06-22 16:00:52 +0300

    shesh

---
+++ kattis/cpp/Das-Blinkenlights.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace::std;
+
+int main()
+{
+    int p,q,s,zes,a,b;
+    cin >> p >> q >> s;
+    a=p;
+    b=q;
+    
+    while(q != 0)
+    {
+        zes = p % q;
+        p = q;
+        q = zes;
+
+
+    }
+    if( ( a / p) * b <= s )
+    {
+        cout << "yes" << endl;
+    }
+    else
+    {
+        cout << "no" << endl;
+    }
+    return 0;
+}
---
+++ kattis/cpp/Prsteni.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace::std;
+
+int main()
+{
+    int n,s,s1,a,b,r;
+    cin >> n;
+    cin >> s;
+   
+    for (int i = 1;i < n;i++)
+    {
+        cin >> s1;
+        a = s;
+        b = s1;
+       
+        while(b != 0)
+        {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        cout << s / a << "/" << s1 / a << endl;
+    }
+
+
+
+
+
+}
