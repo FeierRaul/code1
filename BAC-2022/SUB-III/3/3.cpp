018bd86b4fd43f6e1e871bb5ba1923e943fe3eb1
Feier Raul <raul.feier.9a.2021@cnaibrad.ro>
2022-06-27 12:42:54 +0300

    shesh

---
+++ BAC-2022/SUB-III/3/3.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <cstdio>
+
+using namespace std;
+
+ifstream cin("bac.txt");
+
+int main()
+{
+    int x, y, n0 = -1, n, res = 0;
+    cin >> x >> y;
+
+    while (cin >> n)
+    {
+        if (n0 != n && n >= x && n <= y)
+        {
+            res++;
+        }
+
+        n0 = n;
+    }
+
+    printf("%d\n", res);
+    
+    return 0;
+}
